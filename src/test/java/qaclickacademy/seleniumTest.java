f6155a649a53e06e67e0d8a5f8a993d2ae2fa5b7
supriya-cpu <smahajan2025@gmail.com>
2019-06-23 14:40:39 +0530

    first commit

---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest{
+
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementUI()
+	{
+		System.out.println("ElementUI");
+	}
+}
